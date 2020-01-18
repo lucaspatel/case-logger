b1f52b6187384a8c395fba6a3c05e83265aadaf8
Jacob Patel <jseanpatel@gmail.com>
2020-01-17 21:28:25 -0800

    added block-line-group component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout>
     <div class="flex flex-wrap justify-center bg-gray-200">
-      <div class="w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/3 px-2 bg-white">
+      <div class="w-full px-2 bg-white sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/3">
         <header id="header">
-          <h1 class="text-4xl mx-1 py-8">Case Logger</h1>
+          <h1 class="py-8 mx-1 text-4xl">Case Logger</h1>
         </header>
         <form class="flex flex-col">
           <section class="flex flex-col mb-8" id="patient">
@@ -22,42 +22,19 @@
               <form-input title="Facility" :model="facility"></form-input>
             </div>
           </section>
-          <section class="flex flex-col mb-8" id="procedure">
-            <p class="mx-1">Procedure</p>
-            <div class="flex flex-row">
-              <form-input title="Anesthesia Start" :model="anesthesiaStart"></form-input>
-              <form-input title="Anesthesia End" :model="anesthesiaEnd"></form-input>
-            </div>
-            <div class="block-template">
-              <div class="flex flex-row">
-                <form-input title="Block" :model="blockNum"></form-input>
-              </div>
-              <div class="flex flex-row">
-                <form-input title="Block Start" :model="blockStart"></form-input>
-                <form-input title="Block End" :model="blockEnd"></form-input>
-              </div>
-            </div>
-            <div class="line-template">
-              <div class="flex flex-row">
-                <form-input title="Line" :model="lineNum"></form-input>
-              </div>
-              <div class="flex flex-row">
-                <form-input title="Line Start" :model="lineStart"></form-input>
-                <form-input title="Line End" :model="lineEnd"></form-input>
-              </div>
-            </div>
-            <div class="inline-flex flex-row">
-              <textarea
-                :model="notes"
-                class="w-full h-24 m-1 mb-8 pl-4 pt-3 border border-gray-400 rounded"
-                placeholder="Additional Notes"
-              ></textarea>
-            </div>
-          </section>
+          <block-line-group></block-line-group>
+          <block-line-group></block-line-group>
+          <div class="flex-row">
+            <textarea
+              :model="notes"
+              class="w-full h-24 pt-3 pl-4 m-1 mb-8 border border-gray-400 rounded"
+              placeholder="Additional Notes"
+            ></textarea>
+          </div>
           <section class="flex flex-col" id="controls">
             <div class="flex justify-center">
-              <v-button class="m-1 w-1/2">Add Block </v-button>
-              <v-button class="m-1 w-1/2">Add Line</v-button>
+              <v-button class="w-1/2 m-1">Add Block</v-button>
+              <v-button class="w-1/2 m-1">Add Line</v-button>
             </div>
             <v-button class="m-1">Save</v-button>
           </section>
@@ -70,11 +47,13 @@
 <script>
 import FormInput from "~/components/FormInput.vue";
 import FormButton from "~/components/Button.vue";
+import BlockLineGroup from "~/components/BlockLineGroup.vue";
 
 export default {
   components: {
     FormInput: FormInput,
-    "v-button": FormButton
+    "v-button": FormButton,
+    BlockLineGroup: BlockLineGroup
   },
   data: {},
   metaInfo: {
